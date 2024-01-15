cdb0a4e5c35a4c6eb5575ff269f419067e24575f
Caius Scipio <caiuscipio@gmail.com>
2024-01-15 09:40:50 +0900

    Updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Footer() {
+  
   return (
     <div className="container footer clearfix">
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <Link to="home" className="nav-link px-2 text-muted">
+            <Link
+              to="home"
+              className="nav-link px-2 text-muted"
+            >
               Home
             </Link>
           </li>
